33dbb18970a1af76f1cc0ef96c404a36a575788d
HimanshuRathod18 <60592420+HimanshuRathod18@users.noreply.github.com>
2021-06-27 18:34:43 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <html>
+          <b>Shape AI</b>
+        </html>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1 className="note h1">JavaScript and React.JS</h1>
+      <p className="note p">
+        A basic ShapeAI bootcamp conducted by Shaurya helped me understand the
+        basics and finally get started with JavaScript. It was one of the most
+        easy flowing introduction for me.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
